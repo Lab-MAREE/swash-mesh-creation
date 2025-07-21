a7d06a9a507e3c3b907cb054ab1369d14e33c734
Antoine Lefebvre-Brossard <antoinelb@protonmail.com>
2025-07-21 11:45:05 -0400

    Refactor CLI from argparse to Typer and restructure main functionality
    - Replace argparse CLI with Typer-based interface
    - Add create and apply commands with short aliases (c, a)
    - Restructure main.py with create_mesh and apply_mesh functions
    - Add typer dependency to pyproject.toml
    - Add example bathymetry file for 1D wave channel
    - Update package __init__.py to expose main functions
    - Remove old swash.py module

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+__all__ = [
+    "apply_mesh",
+    "create_mesh",
+]
+
+from .main import apply_mesh, create_mesh
--- src/cli.py
+++ src/cli.py
@@ -1,10 +1,12 @@
-import argparse
+from pathlib import Path
+
+import typer
 
 from . import main
 
-############
-# external #
-############
+##########
+# public #
+##########
 
 
 def run_cli() -> None:
@@ -13,71 +15,54 @@ def run_cli() -> None:
 
     This function initializes the CLI and runs it.
     """
-    parser = _init_cli()
-    args = parser.parse_args()
-
-    if args.input_file is None:
-        parser.print_help()
-        return
-
-    main.convert_files(
-        args.input_file,
-        args.gauges,
-        args.other_input,
-        output_dir=args.output_dir,
-        in_place=args.in_place,
-    )
+    cli = _init_cli()
+    cli()
 
 
-############
-# internal #
-############
+###########
+# private #
+###########
 
 
-def _init_cli() -> argparse.ArgumentParser:
+def _init_cli() -> typer.Typer:
     """
-    Initialize the CLI application with arguments.
+    Initialize the CLI application with commands.
 
     Returns
     -------
-    argparse.ArgumentParser
-        Configured ArgumentParser CLI object
+    typer.Typer
+        Configured Typer CLI object with registered commands
     """
-    parser = argparse.ArgumentParser(
-        description="Transform SWASH input files from cartesian coordinates "
-        + "to unstructured meshes",
-        add_help=True,
+    cli = typer.Typer(
+        context_settings={"help_option_names": ["-h", "--help"]},
+        pretty_exceptions_enable=False,
+        pretty_exceptions_show_locals=False,
     )
-    parser.add_argument(
-        "input_file", type=str, nargs="?", help="SWASH INPUT file"
-    )
-    parser.add_argument(
-        "output_dir",
-        type=str,
-        nargs="?",
-        help="Optional directory where to save the modified files. Must be given if "
-        + "not --in-place.",
-    )
-    parser.add_argument(
-        "-g",
-        "--gauges",
-        type=str,
-        default=None,
-        help="File containing gauge positions (will try to read from INPUT "
-        + "if not given)",
-    )
-    parser.add_argument(
-        "-i",
-        "--other-input",
-        nargs="*",
-        default=[],
-        help="Other input files with a series or grid of values",
-    )
-    parser.add_argument(
-        "--in-place",
-        action="store_true",
-        default=False,
-        help="If all input files should be directly modified instead of a "
-        + "copy created elsewhere",
-    )
-    return parser
+    cli.command("create")(_create_mesh)
+    cli.command("c", hidden=True)(_create_mesh)
+    cli.command("apply")(_apply_mesh)
+    cli.command("a", hidden=True)(_apply_mesh)
+    return cli
+
+
+def _create_mesh(
+    bathymetry_file: str = typer.Argument(..., help="Bathymetry file"),
+) -> None:
+    bathymetry_file_ = Path(bathymetry_file)
+    main.create_mesh(bathymetry_file_)
+
+
+def _apply_mesh(
+    mesh_file: str = typer.Argument(..., help="Mesh file"),
+    input_files: list[str] = typer.Argument(
+        ...,
+        help="Input files to apply the mesh to (can be the INPUT file, gauge positions file or other cgrid files)",
+    ),
+    in_place: bool = typer.Option(
+        False,
+        help="If the files should be directly modified or if a copy in the format `mesh_{file}` should be created.",
+    ),
+) -> None:
+    mesh_file_ = Path(mesh_file)
+    input_files_ = [Path(file) for file in input_files]
+    main.apply_mesh(mesh_file_, input_files_, in_place=in_place)
